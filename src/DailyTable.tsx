a0b611e77bf6da0bda2363485d2b51ba804edd5c
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-05-03 12:00:26 +0800

    use antdesign

--- src/DailyTable.tsx
+++ src/DailyTable.tsx
@@ -1,5 +1,5 @@
-import { useReactTable } from '@tanstack/react-table';
-import { Suspense } from 'react';
+import { Table } from 'antd';
+import { Suspense, useMemo } from 'react';
 
 interface DailyTableProps {
   year?: number;
@@ -35,21 +35,74 @@ function getData(year: number) {
 }
 */
 
+type Question = {
+  "日期": string;
+  "题号": number | string;
+  "题目名称": string;
+  "难度": "简单" | "中等" | "困难",
+  "rating": "1155",
+  "做题情况": "自己做出" | "看思路写出" | "CV后没看懂" | "CV后看懂",
+  "算法和数据结构": string | string[],
+  "学到的内容": string | string[],
+  "没看懂的内容": string | string[]
+}
 
 import data2023 from "./assets/2023.json";
 export default function DailyTable(props: DailyTableProps) {
   const { year = 2023, month = 0 } = props;
   // const data = getData(year).read();
   let data = null;
-  // const options = {};
-  // const table = useReactTable(options);
   switch (year) {
     case 2023: data = data2023; break;
     default: break;
   }
+
+  const columns = [
+    {
+      title: '日期',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '题号',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '题目名称',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: "难度",
+    },
+    {
+      title: "rating"
+    },
+    {
+      title: "做题情况"
+    },
+    {
+      title: "算法和数据结构"
+
+    },
+    {
+      title: "学到的内容"
+
+    },
+    {
+      title: "没看懂的内容"
+
+    }
+  ];
+
   return (
-    <ul>
-      {data ? data.daily.month[0].map((v: any, k: any) => <li key={k}>{v["题号"]}</li>) : <p>no statistics</p>}
-    </ul>
+    <>
+      <Table dataSource={data?.daily.month[month]} columns={columns}></Table>
+      <ul>
+        {data ? data.daily.month[0].map((v: any, k: any) => <li key={k}>{v["题号"]}</li>) : <p>no statistics</p>}
+      </ul>
+
+    </>
   )
 }
