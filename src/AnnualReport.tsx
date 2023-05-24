b7ede45f9ceb8afb7c630252725c19218427bee9
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-05-24 14:36:08 +0800

    feat: add annual report

---
+++ src/AnnualReport.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  tel: string;
+  phone: number;
+  address: string;
+}
+
+// In the fifth row, other columns are merged into first column
+// by setting it's colSpan to be 0
+const sharedOnCell = (_: DataType, index: number) => {
+  if (index === 1) {
+    return { colSpan: 0 };
+  }
+
+  return {};
+};
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'RowHead',
+    dataIndex: 'key',
+    rowScope: 'row',
+  },
+
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    tel: '0571-22098909',
+    phone: 18889898989,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    tel: '0571-22098333',
+    phone: 18889898888,
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    tel: '0575-22098909',
+    phone: 18900010002,
+    address: 'Sydney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 18,
+    tel: '0575-22098909',
+    phone: 18900010002,
+    address: 'London No. 2 Lake Park',
+  },
+  {
+    key: '5',
+    name: 'Jake White',
+    age: 18,
+    tel: '0575-22098909',
+    phone: 18900010002,
+    address: 'Dublin No. 2 Lake Park',
+  },
+];
+
+export default function AnnualReport() {
+  return <Table columns={columns} dataSource={data} bordered />;
+}
