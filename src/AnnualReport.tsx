8b731fc89edf752b80ead109529e373699d07678
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-05-27 14:51:43 +0800

    feat: add annual table

--- src/AnnualReport.tsx
+++ src/AnnualReport.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { Table } from 'antd';
 import { DailyProps } from './constants';
 
-const columns = [
-  {
-    title: 'RowHead',
-    dataIndex: 'key',
-    rowScope: 'row',
-  },
-  {
-    title: '1',
-    dataIndex: '1',
-    key: '1',
-  },
-];
 
 import data2023 from "./assets/2023.json";
 import DifficultyGraph from './components/PieGraph';
+import AnnualTable from './AnnualTable';
 
 export default function AnnualReport(props: DailyProps) {
   const { year = 2023 } = props;
@@ -31,36 +19,59 @@ export default function AnnualReport(props: DailyProps) {
   const month_situations = [];
   const year_difficulty = [{ type: "困难", value: 0 }, { type: "中等", value: 0 }, { type: "简单", value: 0 }];
   const year_situation = [{ type: "自己做出", value: 0 }, { type: "看思路写出", value: 0 }, { type: "看懂答案", value: 0 }, { type: "没看懂答案", value: 0 }];
+  const annual_info: any[] = [{ "info": "困难", }, { "info": "中等", }, { "info": "简单", }, { "info": "自己做出" }, { "info": "看思路写出" }, { "info": "看懂答案" }, { "info": "没看懂答案" }];
+  // const year_info = [];
+
 
   jsonData?.daily.month.forEach((m, idx) => {
     const difficulty = [{ type: "困难", value: 0 }, { type: "中等", value: 0 }, { type: "简单", value: 0 }];
     const situation = [{ type: "自己做出", value: 0 }, { type: "看思路写出", value: 0 }, { type: "看懂答案", value: 0 }, { type: "没看懂答案", value: 0 }];
+    annual_info.forEach((mon) => {
+      mon[`${idx + 1}月`] = 0;
+    })
+
     m.forEach((d) => {
       if (d.difficulty === "困难") {
         difficulty[0].value++;
+        annual_info[0][`${idx + 1}月`]++;
       }
       else if (d.difficulty === "中等") {
         difficulty[1].value++;
+        annual_info[1][`${idx + 1}月`]++;
       }
       else {
         difficulty[2].value++;
+        annual_info[2][`${idx + 1}月`]++;
       }
 
       if (d.situation === "自己做出") {
         situation[0].value++;
+        annual_info[3][`${idx + 1}月`]++;
       }
       else if (d.situation === "看思路写出") {
         situation[1].value++;
+        annual_info[4][`${idx + 1}月`]++;
       }
       else if (d.situation === "看懂答案") {
         situation[2].value++;
+        annual_info[5][`${idx + 1}月`]++;
       }
       else {
         situation[3].value++;
+        annual_info[6][`${idx + 1}月`]++;
       }
     })
     month_difficulties.push(difficulty);
     month_situations.push(situation);
+    // const month_info: any = {};
+    // difficulty.forEach(({ type, value }) => {
+    //   month_info[type] = value;
+    // })
+    // situation.forEach(({ type, value }) => {
+    //   month_info[type] = value;
+    // })
+    // year_info.push(month_info);
+
     year_difficulty[0].value += difficulty[0].value;
     year_difficulty[1].value += difficulty[1].value;
     year_difficulty[2].value += difficulty[2].value;
@@ -70,8 +81,10 @@ export default function AnnualReport(props: DailyProps) {
     year_situation[3].value += situation[3].value;
   })
 
+
   return (
     <>
+      <AnnualTable data={annual_info} />
       <div>
         <div className="show-graph">
           <DifficultyGraph data={year_situation} />
---
+++ src/AnnualTable.tsx
@@ -0,0 +1,124 @@
+import { Table } from 'antd';
+
+
+export default function AnnualTable(props: { data: any[] }) {
+  const { data } = props;
+
+  const columns: any[] = [
+    {
+      title: '',
+      dataIndex: 'info',
+      rowScope: 'row',
+    },
+    {
+      title: '1月',
+      dataIndex: '1月',
+      key: '1',
+    },
+    {
+      title: '2月',
+      dataIndex: '2月',
+      key: '2',
+    },
+    {
+      title: '3月',
+      dataIndex: '1月',
+      key: '3',
+    },
+    {
+      title: '4月',
+      dataIndex: '4月',
+      key: '4',
+    },
+    {
+      title: '5月',
+      dataIndex: '5月',
+      key: '5',
+    },
+    {
+      title: '6月',
+      dataIndex: '6月',
+      key: '6',
+    },
+    {
+      title: '7月',
+      dataIndex: '7月',
+      key: '7',
+    },
+    {
+      title: '8月',
+      dataIndex: '8月',
+      key: '8',
+    },
+    {
+      title: '9月',
+      dataIndex: '9月',
+      key: '9',
+    },
+    {
+      title: '10月',
+      dataIndex: '10月',
+      key: '10',
+    },
+    {
+      title: '11月',
+      dataIndex: '11月',
+      key: '11',
+    },
+    {
+      title: '12月',
+      dataIndex: '12月',
+      key: '12',
+    },
+  ];
+
+  // rotate version
+
+  // const columns = [
+  //   {
+  //     title: '月份',
+  //     dataIndex: 'month',
+  //     key: 'month',
+  //   },
+  //   {
+  //     title: '困难',
+  //     dataIndex: '困难',
+  //     key: 'hard',
+  //   },
+  //   {
+  //     title: '中等',
+  //     dataIndex: '中等',
+  //     key: 'medium',
+  //   },
+  //   {
+  //     title: '简单',
+  //     dataIndex: '简单',
+  //     key: 'easy',
+  //   },
+  //   {
+  //     title: '自己做出',
+  //     dataIndex: '自己做出',
+  //     key: 'hard',
+  //   },
+  //   {
+  //     title: '看思路写出',
+  //     dataIndex: '看思路写出',
+  //     key: 'hard',
+  //   },
+  //   {
+  //     title: '看懂答案',
+  //     dataIndex: '看懂答案',
+  //     key: 'hard',
+  //   },
+  //   {
+  //     title: '没看懂答案',
+  //     dataIndex: '没看懂答案',
+  //     key: 'hard',
+  //   },
+  // ]
+  return (
+    <>
+      <Table dataSource={data} columns={columns}></Table>
+    </>
+  )
+}
